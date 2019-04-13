33dd6f8e73cc90349a9764de8693bb4c8784cc54
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-13 11:19:51 +0100

    Add mock data as a flat file (products.js)

---
+++ src/App/helpers/products.js
@@ -0,0 +1,82 @@
+const products = {
+  product1: {
+    productName: "Almond Toe Court Shoes, Patent Black",
+    productCategory: "Women's Footwear",
+    productPrice: 9900,
+    productQuantity: 5,
+  },
+  product2: {
+    productName: "Suede Shoes, Blue",
+    productCategory: "Women's Footwear",
+    productPrice: 4200,
+    productQuantity: 4,
+  },
+  product3: {
+    productName: "Leather Driver Saddle Loafers, Tan",
+    productCategory: "Men's Footwear",
+    productPrice: 3400,
+    productQuantity: 12,
+  },
+  product4: {
+    productName: "Flip Flops, Red",
+    productCategory: "Men's Footwear",
+    productPrice: 1900,
+    productQuantity: 6,
+  },
+  product5: {
+    productName: "Flip Flops, Blue",
+    productCategory: "Men's Footwear",
+    productPrice: 1900,
+    productQuantity: 0,
+  },
+  product6: {
+    productName: "Gold Button Cardigan, Black",
+    productCategory: "Women’s Casualwear",
+    productPrice: 16700,
+    productQuantity: 6,
+  },
+  product7: {
+    productName: "Cotton Shorts, Medium Red",
+    productCategory: "Women’s Casualwear",
+    productPrice: 3000,
+    productQuantity: 5,
+  },
+  product8: {
+    productName: "Fine Stripe Short Sleeve Shirt, Grey",
+    productCategory: "Men’s Casualwear",
+    productPrice: 4999,
+    productQuantity: 9,
+  },
+  product9: {
+    productName: "Fine Stripe Short Sleeve Shirt, Green",
+    productCategory: "Men’s Casualwear",
+    productPrice: 3999,
+    productQuantity: 9,
+  },
+  product10: {
+    productName: "Sharkskin Waistcoat, Charcoal",
+    productCategory: "Men’s Formalwear",
+    productPrice: 7500,
+    productQuantity: 2,
+  },
+  product11: {
+    productName: "Lightweight Patch Pocket Blazer, Deer",
+    productCategory: "Men’s Formalwear",
+    productPrice: 17550,
+    productQuantity: 1,
+  },
+  product11: {
+    productName: "Bird Print Dress, Black",
+    productCategory: "Women’s Formalwear",
+    productPrice: 27000,
+    productQuantity: 10,
+  },
+  product12: {
+    productName: "Mid Twist Cut­Out Dress, Pink",
+    productCategory: "Women’s Formalwear",
+    productPrice: 54000,
+    productQuantity: 5,
+  },
+}
+
+export default products

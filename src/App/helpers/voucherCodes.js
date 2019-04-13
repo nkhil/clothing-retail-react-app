a9339b86c79b5ec886e154b0a2a8518eb737bb21
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-13 20:13:03 +0100

    Add voucherCodes.js as mock voucher codes

---
+++ src/App/helpers/voucherCodes.js
@@ -0,0 +1,8 @@
+// Change the values on lines 2, 3 and 4 to change the discount codes.
+const five = { code: "FIVEOFF", discountAmount: 500 }
+const ten = { code: "TENOFF", discountAmount: 1000 }
+const fifteen = { code: "FIFTEENOFF", discountAmount: 1500 }
+
+const VoucherCodes = [five, ten, fifteen]
+
+export default VoucherCodes

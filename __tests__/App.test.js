eeec65269dd0c5dadf2344b698ba4c536a2c8443
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-14 00:39:35 +0100

    Remove App.test.js beforeEach() method

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -6,13 +6,13 @@ import renderer from "react-test-renderer"
 require("react-router-dom")
 jest.mock("react-router-dom")
 
-let app
-
-beforeEach(() => {
-  app = renderer.create(<App />).toJSON()
+test("App renders without crashing", () => {
+  const div = document.createElement("div")
+  ReactDOM.render(<App />, div)
 })
 
 test("App renders correctly", () => {
+  const app = renderer.create(<App />).toJSON()
   expect(app).toMatchSnapshot()
 })
 

f931b8f615976fd36b951080e2d914346f2172ed
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-14 22:32:44 +0100

    Add additional test for removeFromCart()

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -85,6 +85,15 @@ test("removeFromCart()", () => {
   expect(componentInstance.state.shoppingCart).toEqual({})
 })
 
+test("removeFromCart() and delete record", () => {
+  const component = renderer.create(<App />)
+  const componentInstance = component.getInstance()
+  componentInstance.addToCart("product1")
+  componentInstance.addToCart("product1")
+  componentInstance.removeFromCart("product1")
+  expect(componentInstance.state.shoppingCart).toEqual({ product1: 1 })
+})
+
 test("_calculateTotal()", () => {
   const component = renderer.create(<App />)
   const componentInstance = component.getInstance()
@@ -184,4 +193,31 @@ test("setActiveVoucherCode()", () => {
   componentInstance.state.voucherCodes = [mockDiscountCode]
   componentInstance.setActiveVoucherCode("TEST")
   expect(componentInstance.state.activeVoucherCode).toEqual("TEST")
+})
+
+test("applyVoucherCode() when code is valid", () => {
+  const component = renderer.create(<App />)
+  const componentInstance = component.getInstance()
+  const mockDiscountCode = {
+    code: "TEST",
+    discountAmount: 500,
+    minimumSpend: 0,
+  }
+  componentInstance.state.voucherCodes = [mockDiscountCode]
+  componentInstance.applyVoucherCode("TEST")
+  expect(componentInstance.state.activeVoucherCode).toEqual("TEST")
+})
+
+test("applyVoucherCode() when code is invalid", () => {
+  const component = renderer.create(<App />)
+  const componentInstance = component.getInstance()
+  const mockDiscountCode = {
+    code: "TEST",
+    discountAmount: 500,
+    minimumSpend: 0,
+  }
+  componentInstance.state.voucherCodes = [mockDiscountCode]
+  jest.spyOn(window, "alert").mockImplementation(() => {})
+  componentInstance.applyVoucherCode("A")
+  expect(window.alert).toBeCalledWith("Invalid discount code")
 })

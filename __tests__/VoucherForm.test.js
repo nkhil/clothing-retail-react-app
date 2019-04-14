d6e5f7dbef5ee85cc3a798ada8eab6236b6871f2
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-14 04:54:34 +0100

    Add renders without crashing test for VoucherForm

--- __tests__/VoucherForm.test.js
+++ __tests__/VoucherForm.test.js
@@ -2,9 +2,15 @@ import "@babel/polyfill"
 import React from "react"
 import VoucherForm from "../src/App/components/VoucherForm"
 import renderer from "react-test-renderer"
+import ReactDOM from "react-dom"
 
 require("react-router-dom")
 jest.mock("react-router-dom")
+
+test("VoucherForm renders without crashing", () => {
+  const div = document.createElement("div")
+  ReactDOM.render(<VoucherForm />, div)
+})
 
 test("VoucherForm renders correctly", () => {
   const voucherForm = renderer.create(<VoucherForm />).toJSON()

fbb952d3be064ebcc0b3d0178128a9fd45498b25
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-14 05:25:06 +0100

    Add test for loadProductsIntoState()

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -17,6 +17,26 @@ test("App renders correctly", () => {
   expect(app).toMatchSnapshot()
 })
 
+test("loadProductsIntoState()", () => {
+  const mockProducts = {
+    product7: {
+      productName: "Cotton Shorts, Medium Red",
+      productCategory: "Women’s Casualwear",
+      productPrice: 3000,
+      productQuantity: 5,
+    },
+    product8: {
+      productName: "Fine Stripe Short Sleeve Shirt, Grey",
+      productCategory: "Men’s Casualwear",
+      productPrice: 4999,
+      productQuantity: 9,
+    },
+  }
+  const component = renderer.create(<App />)
+  const componentInstance = component.getInstance()
+  componentInstance.loadProductsIntoState(mockProducts)
+  expect(componentInstance.state.products).toEqual(mockProducts)
+})
 test("addToCart()", () => {
   const shoppingBasket = { product1: 1 }
   const component = renderer.create(<App />)

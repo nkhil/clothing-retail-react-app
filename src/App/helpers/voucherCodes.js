084570835a3a05b1a6fa2dbc5e4da267fbae148d
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-14 05:28:08 +0100

    Modify schema for local voucherCodes file

--- src/App/helpers/voucherCodes.js
+++ src/App/helpers/voucherCodes.js
@@ -1,7 +1,18 @@
-// Change the values on lines 2, 3 and 4 to change the discount codes.
-const five = { code: "FIVEOFF", discountAmount: 500 }
-const ten = { code: "TENOFF", discountAmount: 1000 }
-const fifteen = { code: "FIFTEENOFF", discountAmount: 1500 }
+const five = {
+  code: "FIVEOFF",
+  discountAmount: 500,
+  minimumSpend: 0,
+}
+const ten = {
+  code: "TENOFF",
+  discountAmount: 1000,
+  minimumSpend: 5000,
+}
+const fifteen = {
+  code: "FIFTEENOFF",
+  discountAmount: 1500,
+  minimumSpend: 7500,
+}
 
 const voucherCodes = [five, ten, fifteen]
 

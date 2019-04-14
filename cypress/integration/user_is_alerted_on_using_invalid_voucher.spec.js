e9f12eed8e6c843729e9bb80be4352b486ddd705
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-14 12:48:20 +0100

    Add integration tests for invalid voucher code

--- cypress/integration/user_is_alerted_on_using_invalid_voucher.spec.js
+++ cypress/integration/user_is_alerted_on_using_invalid_voucher.spec.js
@@ -4,7 +4,7 @@ context("User visits the app", () => {
   })
 
   describe("User is alerted", () => {
-    it("Alerts use when an invalid discount code is used", () => {
+    it("Alerts user on invalid discount code - wrong voucher code", () => {
       const stub = cy.stub()
       cy.on("window:alert", stub)
       cy.get(".product1-button").click({ force: true })
@@ -12,6 +12,54 @@ context("User visits the app", () => {
         cy.get("#voucher-form")
           .find('[type="text"]')
           .type("a")
+        cy.get("#voucher-form")
+          .submit()
+          .then(() => {
+            expect(stub.getCall(0)).to.be.calledWith("Invalid discount code")
+          })
+      })
+    })
+
+    it("Alerts user on invalid discount code - no footwear items", () => {
+      const stub = cy.stub()
+      cy.on("window:alert", stub)
+      cy.get(".product6-button").click({ force: true })
+      cy.get("#shoppingCart").within($shoppingCart => {
+        cy.get("#voucher-form")
+          .find('[type="text"]')
+          .type("fifteenoff")
+        cy.get("#voucher-form")
+          .submit()
+          .then(() => {
+            expect(stub.getCall(0)).to.be.calledWith("Invalid discount code")
+          })
+      })
+    })
+
+    it("Alerts user on invalid discount code - no minimum spend", () => {
+      const stub = cy.stub()
+      cy.on("window:alert", stub)
+      cy.get(".product4-button").click({ force: true })
+      cy.get("#shoppingCart").within($shoppingCart => {
+        cy.get("#voucher-form")
+          .find('[type="text"]')
+          .type("fifteenoff")
+        cy.get("#voucher-form")
+          .submit()
+          .then(() => {
+            expect(stub.getCall(0)).to.be.calledWith("Invalid discount code")
+          })
+      })
+    })
+
+    it("Alerts user on invalid discount code - no minimum spend 2", () => {
+      const stub = cy.stub()
+      cy.on("window:alert", stub)
+      cy.get(".product3-button").click({ force: true })
+      cy.get("#shoppingCart").within($shoppingCart => {
+        cy.get("#voucher-form")
+          .find('[type="text"]')
+          .type("tenoff")
         cy.get("#voucher-form")
           .submit()
           .then(() => {
